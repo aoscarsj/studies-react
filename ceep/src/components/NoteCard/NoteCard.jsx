50e6b681cda524f7cccd3323c082b5399a9f30ea
Artur Oscar <arturoscar3@gmail.com>
2021-12-15 21:40:27 -0300

    Add delete icon in note

--- ceep/src/components/NoteCard/NoteCard.jsx
+++ ceep/src/components/NoteCard/NoteCard.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import './style.css';
-
+import { ReactComponent as DeleteSVG } from '../../assets/img/delete.svg';
 class NoteCard extends Component {
   render() {
     return (
       <section className='note-card'>
         <header className='note-card_header'>
           <h3 className='note-card_title'>{this.props.title}</h3>
+          <DeleteSVG
+            onClick={() => {
+              console.log('click');
+            }}
+          />
         </header>
         <p className='note-card_text'>{this.props.text}</p>
       </section>
