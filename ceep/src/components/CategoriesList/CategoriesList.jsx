0a30c482dec232c0af3675da154da5081105da20
Artur Oscar <arturoscar3@gmail.com>
2021-12-21 22:35:51 -0300

    add css in categorylist

--- ceep/src/components/CategoriesList/CategoriesList.jsx
+++ ceep/src/components/CategoriesList/CategoriesList.jsx
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
-
+import './style.css';
 class CategoriesList extends React.Component {
   _handleInputEvent(event) {
     if (event.key === 'Enter') {
       let category = event.target.value;
       this.props.addCategory(category);
+      event.target.value = '';
     }
   }
 
   render() {
     return (
-      <section>
-        <ul>
+      <section className='categories-list'>
+        <ul className='categories-list_list'>
           {this.props.categories.map((category, index) => {
-            return <li key={index}>{category}</li>;
+            return (
+              <li key={index} className='categories-list_item'>
+                {category}
+              </li>
+            );
           })}
         </ul>
         <input
           type='text'
           placeholder='Add category'
           onKeyUp={this._handleInputEvent.bind(this)}
+          className='categories-list_input'
         />
       </section>
     );
