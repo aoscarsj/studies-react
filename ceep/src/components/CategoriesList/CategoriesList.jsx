71c2fb1a1019f61e4932b417c6593f53cca71999
Artur Oscar <arturoscar3@gmail.com>
2021-12-21 22:27:09 -0300

    dynamically adding the category list

--- ceep/src/components/CategoriesList/CategoriesList.jsx
+++ ceep/src/components/CategoriesList/CategoriesList.jsx
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
 
 class CategoriesList extends React.Component {
+  _handleInputEvent(event) {
+    if (event.key === 'Enter') {
+      let category = event.target.value;
+      this.props.addCategory(category);
+    }
+  }
+
   render() {
     return (
       <section>
         <ul>
-          <li>test</li>
-          <li>test</li>
-          <li>test</li>
-          <li>test</li>
-          <li>test</li>
+          {this.props.categories.map((category, index) => {
+            return <li key={index}>{category}</li>;
+          })}
         </ul>
-        <input type='text' />
+        <input
+          type='text'
+          placeholder='Add category'
+          onKeyUp={this._handleInputEvent.bind(this)}
+        />
       </section>
     );
   }
